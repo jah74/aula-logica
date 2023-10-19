60457fa4e24da5d6e520d109396204ff95f5e493
Jose <jarrieta74@hotmai.com>
2023-10-18 23:59:30 -0300

    meu primeiro commit

---
+++ tarefa-1.js
@@ -0,0 +1,31 @@
+
+let dia = "sabado";
+
+switch (dia)
+{case "segunda": console.log("primeiro dia da semana");
+ break
+ case "terça": console.log("segundo dia da semana");
+ break
+ case "quarta": console.log("terceiro dia da semana");
+ break
+ default:
+    console.log("Final de semana! Descanse e divirta-se!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;
